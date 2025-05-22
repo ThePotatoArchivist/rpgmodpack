6d4f8e7b6af20909ce10dc7d25d80334faa3fda9
ThePotatoArchivist <josiahfu@gmail.com>
2025-05-22 15:57:57 -0700

    Add install script

---
+++ run.sh
@@ -0,0 +1,23 @@
+set -e
+set -u
+set -o pipefail
+
+PACK_DIR="$(dirname "$(realpath $0)")"
+
+if [ ! -f .GAME_DIR ]; then
+    read -p "Type path to game dir: " GAME_DIR
+    GAME_DIR="$(realpath "$GAME_DIR")"
+    if [ ! -d "$GAME_DIR/mods" ]; then
+        echo "Could not find $GAME_DIR/mods"
+        exit 1
+    fi
+    echo "$GAME_DIR" > .GAME_DIR
+else
+    read GAME_DIR < .GAME_DIR
+fi
+
+cd "$GAME_DIR"
+
+packwiz-installer -g "$PACK_DIR/pack.toml"
+
+echo "Mods updated!"
